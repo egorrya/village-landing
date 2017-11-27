1fa97a5b4d77a453de9a06daa34186bd7a29c064
Egor Rya <fayncore@gmail.com>
2017-11-28 03:05:52 +0700

    ne smotri
    pasha konkretniy pidor

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,33 +2,11 @@
 
 $(document).ready(function(){
 
-	
-
 	// animate css
 
 	$(".main-nav__items").animated("fadeInLeft");
 	$(".home-section__title").animated("flash");
 	$(".menu__item").animated("slideInLeft");
+	$(".cart__options").animated("slideInRight");
 
-	// counter for 10000 sec
-
-	$('.waiting__counter').each(function() {
-		var $this = $(this),
-		countTo = $this.attr('data-count');
-
-		$({ countNum: $this.text()}).animate({
-			countNum: countTo
-		},
-
-		{
-			duration: 10000000,
-			easing:'linear',
-			step: function() {
-				$this.text(Math.floor(this.countNum));
-			},
-			complete: function() {
-				$this.text(this.countNum);
-			}
-		});
-	});
 });
