d2c9bf7a4ac9fc383f75d30232ecc29da2205f9f
Egor Rya <fayncore@gmail.com>
2017-11-28 18:04:58 +0700

    ready for casechamp

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
 
 	$(".main-nav__items").animated("fadeInLeft");
 	$(".home-section__title").animated("flash");
-	$(".menu__item").animated("slideInLeft");
+	$(".menu__item").animated("fadeInUp");
 	$(".cart__options").animated("slideInRight");
+
+	$('input.ShowOrHide').click(function() {
+
+		var checked = $("input.ShowOrHide:checked");
+
+		if (checked.length == 0) {
+			$("div.ShowOrHide").show();
+		} else {
+			$("div.ShowOrHide").hide();
+			$('div#' + $(this).val()).show();
+		}
+	});
 });
